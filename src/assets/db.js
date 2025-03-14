b1d534e0e7cdb24c9c3345843c6aa144ed5a6722
MaxKorovchenko <max.korovchenko@gmail.com>
2025-03-14 09:25:18 +0200

    update teams 2025 year

--- src/assets/db.js
+++ src/assets/db.js
@@ -1,43 +1,45 @@
 import arsenal from 'assets/images/Arsenal.png';
 import bayern from 'assets/images/Bayern_München.png';
-import atletico from 'assets/images/Atletico_Madrid.png';
+//import atletico from 'assets/images/Atletico_Madrid.png';
 import borussia from 'assets/images/Borussia_Dortmund.png';
-import city from 'assets/images/Manchester_City.png';
+//import city from 'assets/images/Manchester_City.png';
 import real from 'assets/images/Real_Madrid.png';
 import psg from 'assets/images/FC_Paris_Saint-Germain.png';
 import barselona from 'assets/images/FC_Barcelona.png';
+import inter from 'assets/images/FC_Internazionale_Milano.png';
+import villa from 'assets/images/Aston Villa F.C.png';
 
 export const clubs = [
   {
-    title: 'Club Atlético de Madrid',
-    logo: atletico,
-  },
-  {
-    title: 'Borussia Dortmund',
-    logo: borussia,
-  },
-  {
-    title: 'Paris Saint-Germain FC',
-    logo: psg,
-  },
-  {
-    title: 'FC Barcelona',
-    logo: barselona,
-  },
-  {
     title: 'Arsenal FC',
     logo: arsenal,
-  },
-  {
-    title: 'FC Bayern München',
-    logo: bayern,
   },
   {
     title: 'Real Madrid CF',
     logo: real,
   },
   {
-    title: 'Manchester City FC',
-    logo: city,
+    title: 'Paris Saint-Germain FC',
+    logo: psg,
+  },
+  {
+    title: 'Aston Villa FC',
+    logo: villa,
+  },
+  {
+    title: 'FC Barcelona',
+    logo: barselona,
+  },
+  {
+    title: 'Borussia Dortmund',
+    logo: borussia,
+  },
+  {
+    title: 'FC Bayern München',
+    logo: bayern,
+  },
+  {
+    title: 'FC Internazionale Milano',
+    logo: inter,
   },
 ];
